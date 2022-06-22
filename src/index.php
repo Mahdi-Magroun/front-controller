8e55a054c50dc6a274f571a3f1dc8812ba6c5439
Mahdi <mmahdiwebdeveloper@gmail.com>
2022-06-22 01:36:34 +0100

    front * controller

---
+++ src/index.php
@@ -0,0 +1,29 @@
+<?php
+include_once "../vendor/autoload.php";
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+$request= Request::createFromGlobals();
+$response= new Response();
+//$response->setContent($request->getPathInfo());
+//$response->send();
+
+switch ($request->getPathInfo()) {
+    case '/front-controller/page1.php':
+     
+        ob_start();
+        include __DIR__.'/page1.php';
+        $response->setContent(ob_get_clean());
+        break;
+    case '/front-controller/page2.php':
+        ob_start();
+        include __DIR__.'/page2.php';
+        $response->setContent(ob_get_clean());
+        break;
+    default:
+      $response->setContent("page not found");
+      $response->setStatusCode(404);
+        break;
+    
+}
+$response->send();
